a190d4242725c8db54d7b7aad5d5760c7abd47af
Adlai-byte <56060738+Adlai-byte@users.noreply.github.com>
2023-04-16 20:53:24 +0800

    Version 2.0.1

--- app/src/main/java/com/example/ifixit/CUSTOMER_FILES/CustomerMapsActivity.java
+++ app/src/main/java/com/example/ifixit/CUSTOMER_FILES/CustomerMapsActivity.java
@@ -1,8 +1,19 @@
 package com.example.ifixit.CUSTOMER_FILES;
 
+import static com.example.ifixit.R.*;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.ifixit.R;
 import com.example.ifixit.databinding.ActivityCustomerMapsBinding;
@@ -12,9 +23,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.navigation.NavigationView;
 
-public class CustomerMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class CustomerMapsActivity extends AppCompatActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
 
+    //Variables
+    private DrawerLayout drawer;
     private GoogleMap mMap;
     private ActivityCustomerMapsBinding binding;
 
@@ -25,21 +39,66 @@ public class CustomerMapsActivity extends FragmentActivity implements OnMapReady
         binding = ActivityCustomerMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(id.map);
         mapFragment.getMapAsync(this);
+
+
+        Toolbar toolbar = findViewById(id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(id.drawer_layout);
+        NavigationView navigationView = findViewById(id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,toolbar, string.navigation_drawer_open , string.navigation_drawer_close );
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+//        if(savedInstanceState==null){
+//            getSupportFragmentManager().beginTransaction().replace(id.fragment_container,new MessageFragment()).commit();
+//            navigationView.setCheckedItem(id.nav_message);
+//        }
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(id.fragment_container,new ProfileFragment()).commit();
+                break;
+            case id.nav_map:
+                Intent intent = new Intent(CustomerMapsActivity.this,CustomerMapsActivity.class);
+                startActivity(intent);
+                break;
+            case id.nav_message:
+                getSupportFragmentManager().beginTransaction().replace(id.fragment_container,new MessageFragment()).commit();
+                break;
+            case id.nav_notification:
+                getSupportFragmentManager().beginTransaction().replace(id.fragment_container,new NotificationFragment()).commit();
+                break;
+            case id.nav_share:
+                Toast.makeText(this, "Shared", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+
+
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -49,4 +108,5 @@ public class CustomerMapsActivity extends FragmentActivity implements OnMapReady
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
 }
